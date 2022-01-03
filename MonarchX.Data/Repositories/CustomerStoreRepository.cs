02c02c0b2fa0519bdcd4da206b5c66715f296fa1
shahrukh <monarchsystemsales@gmail.com>
2022-01-03 15:04:33 -0600

    new customer record

--- MonarchX.Data/Repositories/CustomerStoreRepository.cs
+++ MonarchX.Data/Repositories/CustomerStoreRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using MonarchX.Data.Models;
@@ -13,6 +14,26 @@ namespace MonarchX.Data
         public CustomerStoreRepository(CustomerStoreDbContext context) : base(context)
         {
 
+        }
+
+        //Saves new customer into the database 
+        public Customer AddNewCustomer(string firstName, string lastName, string address, string? address2, PhoneAttribute phone, string email){
+
+            Customer newCustomer = new Customer(){
+
+                FirstName = firstName,
+                LastName = lastName,
+                StreetAddress = address,
+                //Nullable not required
+                Address2 = address2,
+                PhoneNumber = phone,
+                Email = email
+
+            };
+
+            Db.Customers.Add(newCustomer);
+            var recordsAffected = Db.SaveChanges();
+            return newCustomer;
         }
 
     }}
